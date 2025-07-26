6aaf1f28663d4e6e532b112d5abc39089fd35c80
egork <egorkrasnov123456789@gmail.com>
2025-07-26 22:13:56 +0700

    Intial Commit

---
+++ app/src/main/java/com/example/photoedit/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.photoedit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import coil.transform.*
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Column {
+                    TopAppBar(title = { Text("PhotoEdit") })
+                }
+            }
+        }
+    }
+}
+
+
+
