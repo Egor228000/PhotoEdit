d9d7dcb653ef239f4b8fab1da8e78dffcc57d83d
egork <egorkrasnov123456789@gmail.com>
2025-08-02 01:13:50 +0700

    add screen

---
+++ app/src/main/java/com/example/photoedit/presentation/Collage/CollageScreen.kt
@@ -0,0 +1,12 @@
+package com.example.photoedit.presentation.Collage
+
+import androidx.compose.runtime.Composable
+import com.example.photoedit.presentation.Gallery.GalleryViewModel
+
+
+@Composable
+fun CollageScreen(galleryViewModel: GalleryViewModel) {
+
+
+
+}
