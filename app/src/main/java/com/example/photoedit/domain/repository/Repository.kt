6f2a658a2abade977eb34221b11105ebbaf03f5a
egork <egorkrasnov123456789@gmail.com>
2025-08-06 20:17:05 +0700

    удаление clean архитектуры - из за ненадобности

--- app/src/main/java/com/example/photoedit/domain/repository/Repository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.photoedit.domain.repository
-
-import com.example.photoedit.data.local.entity.ImageUriEntity
-
-interface Repository {
-    suspend fun saveUri(entity: ImageUriEntity)
-    suspend fun getUri(id: Int): ImageUriEntity?
-}
